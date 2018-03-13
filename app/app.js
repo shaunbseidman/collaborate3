a10d22dda1bfbb3af45b028df22cc797d367558e
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-03-13 13:42:52 -0600

    added app.js and created array of numbers to multiply

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const numbers = [69, 420, 66]
+
+function getTotal(poop) {
+  let fartNumber = 1
+  for (i=0;i<poop.length;i++) {
+    fartNumber *= poop[i]
+  }
+  console.log(fartNumber)
+  return fartNumber
+}
+
+getTotal(numbers)
